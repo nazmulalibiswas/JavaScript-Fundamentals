2d407033c1a3c810ce74f4db1d4536f7375d9a13
Nazmul Ali  Biswas <nazmulali8262@gmail.com>
2024-12-11 19:12:14 +0600

    JavaScript Data Types

--- javaScriptfundamental.js
+++ javaScriptfundamental.js
@@ -252,3 +252,425 @@ console.log ("I am programmer")*/
 //var result = 9/5 *temp + 32;
 //alert("Fahrenheit: " + result + "Degree");
 //console.log("Task Complete!");//Now go to javaScriptfundamental.html and check
+
+//-----------------------------------------
+//#Data Types
+//@Numbers
+
+//var x;//Means 64 bit data is allocate in my computer memory
+//x = 3.1416;
+//console.log(x)//Output: 3.1416
+//x = 34; 
+//console.log(x)//Output: 34
+//x = 123e5;
+//console.log(x)//Output: 12300000
+//x=123 * 10**5
+//console.log(x)//Output: 12300000
+//x=123e-5
+//console.log(x)//Output: 0.00123
+
+//"34" + "45"//Output:"3445" // Basically adding the number
+//34 + "45" //Output:"4534" //Basically concatenation the number
+//"45" + 34 //Output: "4534" //Basically concatenation the number
+//34 + 45 + "45" //Output: "7945" //Basically concatenation the number
+//"The result: " + 34 + 45//Output:"The result: 3445"//Basically concatenation the number
+//"25" - "5"//Output:20
+//"25"/"5"//Output:5
+//"25"*"5"//Output:125
+//"25"*5//Output:125
+//"25"%"5"//Output:0
+//"25" + "5"//Output:255 //This is only exception
+//25/0//Output:Infinity
+
+//var y = 25/0
+//console.log(y);//Output:Infinity
+//
+//var x = 0xBB;
+//console.log(x);//Output: 187
+////functionmethodshow
+//x.toString();//Output:187
+//var y = x.toString();
+//console.log(y);//Output: 187
+//
+//x = 3.1416
+//y=x.toString();
+//console.log(y);//Output: "3.1416"
+//
+//var x = 187
+//y = x.toString(2);
+//console.log(y);//Output: "10111011" Converted it from binary
+//y = x.toString(8);
+//console.log(y);//Output: "273" Converted it from Octal
+//y = x.toString(16);
+//console.log(y);//Output: "bb" Converted it from Hexadecimal
+//x = 3.1416;
+//y = x.toPrecision();//Output: "3.1416" 
+//y = x.toPrecision(2);//Output: "3.1" 
+//y = x.toPrecision(3);//Output: "3.14" 
+//y = x.toPrecision(5);//Output: "3.1416"
+//y = x.toPrecision(10);//Output: "3.141600000" 
+//
+//x = 187; //Convert it string
+//y = x.toString();
+//parseInt(y);//Output: "187"
+//var z = parseInt*(y);
+//console.log(z);//Output:187
+//console.log(y);//Output:"3.1416"
+//
+//y="3.1416";
+//z = parseInt(y);//Output: 3
+//z = parseFloat(y);//Output:3.1416
+//
+//x = 3.1416;
+//x.toString(x);//Output:"3.1416"
+//parseFloat(x.toString());//Output: 3.1416
+//
+////isNaN (If number return true otherwise false)
+//var x = "hsjkfhs";
+//isNaN(x);//true
+//var x = 188;
+//isNaN (x)//False
+//var x = "134";
+//isNaN (x)//False
+//var x = "134fsfsa";
+//isNaN (x)//true
+
+//@Strings
+//console.log("Hello World");
+//console.log("Hello '''' '''' World");
+//console.log('Hello World');
+//console.log('Hello """" """" World');
+
+//var x = 'Hello """"""" World';
+//console.log(x);
+
+//var x = "Hello"""""""World"; 
+//console.log(x);//Output: Unexpected String
+
+//var x = "Hello \" World"; 
+//console.log(x);//Output: "Hello " World"
+
+//var x = 'Hello \' World'; 
+//console.log(x);//Output: Hello ' World
+
+//var x = 'Hello \\ World'; 
+//console.log(x);//Output: Hello \ World
+
+//var x = 'Hello \t World'; 
+//console.log(x);//Output: Hello 	 World // tab space continue
+
+/*var x = 'Hello \n World'; 
+console.log(x);//Output: Hello 
+                          World */
+
+//var x = "Hello";
+//var y = "World";
+//var z = x + y;
+//console.log(z);//Output: HelloWorld
+  //If i want to spaces
+//var x = "Hello";
+//var y = " World";
+//var z = x + y;
+//console.log(z);//Output: Hello World
+//console.log(z.length);//Output: 11 //Total number of character
+
+//var x = "Hello \nWorld!";
+//console.log(x.length);//Output:13
+//var z = x.length;
+//console.log(z);//Output:13
+//z = "learnwithnazmulalibiswas".length;
+//console.log(z);//Output:24
+
+//var x = "nazmulalibiswas";
+//console.log(x[0]);//Output:n
+
+//var x = "nazmulalibiswas";
+//console.log(x[1]);//Output:a
+
+//var x = "nazmulalibiswas";
+//console.log(x[2]);//Output:z
+
+//var x = "nazmulalibiswas";
+//console.log(x[16]);//Output:Undefiend
+
+//var x = "nazmulalibiswas";
+//console.log(x.toUpperCase());//Output:NAZMULALIBISWAS
+//z = x.toUpperCase();
+//console.log(z);//Output:NAZMULALIBISWAS
+//z = x.toLowerCase();
+//console.log(z);//Output:nazmulalibiswas
+
+//var x = " nazmulalibiswas ";
+//console.log(x);//Output: nazmulalibiswas //Here first and last line has spaces
+
+//var z = " nazmulalibiswas ";
+//console.log(z.trim());//Output:nazmulalibiswas//Here no spaces
+//console.log(z.slice());//Output:  nazmulalibiswas //no changes
+//console.log(z.slice(7, 10));//Output:ali //When declare parameter
+//console.log(z.slice(-5, -1));//Output:swas //Starting index counting nazmulalibi after that & space index 0 and s index -1 so after counting so -5<-1 where -5s previous index i don't count
+//console.log(z.slice(2));//Output:azmulalibiswas here space index (0) & next (n) are sliced
+//console.log(z.substr(3, 2));//Output:zm here m showing becuse it fixed
+//console.log(z.substr(3));//Output:zmulalibiswas
+//console.log(z.replace("ali","code"));//Output: nazmulcodebiswas 
+
+//var x = "ABC";
+//console.log(x.concat(z, "EFG","123"));//Output:ABC nazmulalibiswas EFG123
+
+//@Booleans
+  //Which is consist only one logic true or false
+
+//let x = 15;
+//let y = 12;
+//console.log(x>y);//Output:true
+//console.log(x<y);//Output:false
+
+//let z = "nazmulalibiswas";
+//console.log(Boolean(z));//Output:true
+
+
+//var t;
+//console.log(t);//Output:Undefined but remember undefined and NaN different case
+//var t;
+//console.log(Boolean(t));//Output:false
+
+//var f;
+//f= null;
+//console.log(Boolean(f));//Output:false
+
+//var k = 10/"H";
+//console.log(k);//Output:NaN
+//var k = 10/"H";
+//console.log(Boolean(k));//Output:false //undefined, null, NaN are not same three are different things
+
+
+//@Array
+//var countries = ["Bangladesh", "USA", "UK"];
+//console.log(countries);//output: [ 'Bangladesh', 'USA', 'UK' ]
+//console.log(countries[1]);//output: USA
+//console.log(countries.length);//output:3
+//countries[1] = "Poland";
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK' ]
+//country = countries[0];
+//console.log(country);//output:Bangladesh
+//country = countries[90];
+//console.log(country);//output:Undefined
+//console.log(countries.length);//output:3
+  //array element includes
+//countries[3] = "Norway";
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway' ]
+//countries[countries.length] = "Sewden"
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sewden' ]
+//countries[4] = "Sweden";
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden' ]
+//countries.push ("china");
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden', 'china' ]
+//countries.pop ();
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden' ] //using pop last element is skip
+//countries.push("china", "Japan");
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden', 'china', 'Japan' ]
+//countries.shift();
+//console.log(countries);//output:[ 'Poland', 'UK', 'Norway', 'Sweden', 'china', 'Japan' ] //It will be skip first element
+//countries.unshift("Bangladesh");
+//console.log(countries);//output:[ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden', 'china', 'Japan' ]
+
+//var numbers = [];
+//console.log(numbers);//Output:[]
+//console.log(numbers.push(12));//Output:1
+//console.log(numbers);//Output:[ 12 ]
+//console.log(numbers.push("two"));//Output: 2 //Here includes [2, "two"]
+
+//var x = "Bangladesh";
+//var y = x.split("");
+//console.log(y); /* Output:[  'B', 'a', 'n', 'g', 'l', 'a', 'd', 'e', 's', 'h']*/
+
+//var y = x.split();
+//console.log(y);//Output:[ 'Bangladesh' ]
+
+//var x = "Hello World!";
+//y = x.split("")
+//console.log(y);/*Output:['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!']*/
+
+//var x = "Bangladesh is a country";
+//y = x.split("n");
+//console.log(y);//Output:[ 'Ba', 'gladesh is a cou', 'try' ] //When runtime compiler got an n thene this portion is spilit
+//y = x.split(" ");
+//console.log(y);//Output:[ 'Bangladesh', 'is', 'a', 'country' ] //Every sentence will be declaring as a array
+//x = "Bangladesh, China, Finland";
+//y = x.split(",");
+//console.log(y);//Output:[ 'Bangladesh', ' China', ' Finland' ]
+  //lets see array convert to string
+//var z = y.toString();
+//console.log(z);//Output:Bangladesh, China, Finland
+  //how to understand using , that
+//var x = "Bangladesh is a country";
+//console.log(x);//Output:Bangladesh, China, Finland
+//y = x.split(" ");
+//console.log(y);//Output:[ 'Bangladesh', 'is', 'a', 'country' ]
+//z = y.toString();
+//console.log(z);//Output:Bangladesh,is,a,country // Here not showing qoatation mark but it is still string
+//z = y.join("/");
+//console.log(z);//Output:Bangladesh/is/a/country
+//z = y.join(" ");
+//console.log(z);//Output:Bangladesh is a country
+
+//console.log (countries);
+//console.log(y);
+//z = x.concat(y);
+//console.log(z);//Output:Bangladesh is a countryBangladesh,is,a,country
+//z = countries.concat(y);
+/*console.log(z);//Output: [
+  'Bangladesh', 'Poland',
+  'UK',         'Norway',
+  'Sweden',     'china',
+  'Japan',      'Bangladesh',
+  'is',         'a',
+  'country'
+  ]*/
+
+//console.log(countries);//Output: [ 'Bangladesh', 'Poland', 'UK', 'Norway', 'Sweden', 'china', 'Japan' ]
+//console.log(countries.sort());//Output: [ 'Bangladesh', 'Japan', 'Norway', 'Poland', 'Sweden', 'UK', 'china' ]
+//console.log(countries.reverse());//Output: [ 'china', 'UK', 'Sweden', 'Poland', 'Norway', 'Japan', 'Bangladesh' ]
+
+//@Objects
+  //Objects is javaScript data types which is working for property
+//var arr = [1,2,3];
+//console.log(arr);//Output: [ 1, 2, 3 ]
+  //property is same like artributes in html so that objects has property & value
+//var student = {name:"Rahim", Age: "36", hometown: "Dhaka"}; //always use carly brackets
+//console.log(student);//Output:{ name: 'Rahim', Age: '36', hometown: 'Dhaka' }
+//console.log(arr[0]);//Output: 1
+//console.log(student["name"]);//Output:Rahim
+//console.log(student["Age"]);//Output:36
+//console.log(student["hometown"]);//Output:Dhaka
+  //Method 2
+//console.log(student.name);//Output:Rahim
+//console.log(student.Age);//Output:36
+//console.log(student.hometown);//Output:Dhaka
+//console.log(arr.length);//Output:3
+
+//student["occupation"] = "Student";
+//console.log(student); //Output:{ name: 'Rahim', Age: '36', hometown: 'Dhaka', occupation: 'Student' } Here are added one property & value
+/*student.village = "Rajapur";
+console.log(student); //Output:{
+  name: 'Rahim',
+  Age: '36',
+  hometown: 'Dhaka',
+  occupation: 'Student',
+  village: 'Rajapur'
+}*/
+  // How to delete any property
+//delete student.village;
+//console.log(student);//Output:{ name: 'Rahim', Age: '36', hometown: 'Dhaka', occupation: 'Student' }
+  //How to new object created
+//student = {};
+//console.log();
+//student.name = "Fahim";
+//student.age = "24";
+//console.log(student);//Output:{ name: 'Fahim', age: '24' }
+
+//@Array and Objects
+  //array & object are mixed
+//var arr = [10, 12, 8, 9];
+//console.log(arr);//Output:[ 10, 12, 8, 9 ]
+//var arr = [10, 12, ["a", "b"], 8, 9];
+//console.log(arr);//Output:[ 10, 12, [ 'a', 'b' ], 8, 9 ]
+//console.log(arr [2]);//Output:[ 'a', 'b' ]
+//console.log(arr [2][0]);//Output:a
+//arr = [1,2,["a", ["word1", "word2"], "b"],7,18];
+//console.log(arr);//Output:[ 1, 2, [ 'a', [ 'word1', 'word2' ], 'b' ], 7, 18 ]
+//console.log(arr[2][1][0]);//Output:word1
+//console.log(arr[2][1][1]);//Output:word2
+//arr = [1,2,["a","b"], {prop1:"Item 1", prop2:"Item 2"}];
+//console.log(arr);//Output:[ 1, 2, [ 'a', 'b' ], { prop1: 'Item 1', prop2: 'Item 2' } ]
+//console.log(arr[3]);//Output:{ prop1: 'Item 1', prop2: 'Item 2' }
+//console.log(arr[3]["prop1"]);//Output:Item 1
+//arr = ["Item 1", "Item 2", {prop1:"Item 1", prop2:[1,2,["a","b"]]}];
+//console.log(arr);//Output:[ 'Item 1', 'Item 2', { prop1: 'Item 1', prop2: [ 1, 2, [Array] ] } ]
+//console.log(arr[2]["prop2"][2][0]);//Output:a
+//console.log(arr[2]);//Output:{ prop1: 'Item 1', prop2: [ 1, 2, [ 'a', 'b' ] ] }
+//console.log(arr[2].prop2);//Output:{ prop1: 'Item 1', prop2: [ 1, 2, [ 'a', 'b' ] ] }
+//console.log(arr[2].prop2[2]);//Output:[ 1, 2, [ 'a', 'b' ] ]
+//console.log(arr[2]["prop2"][2][0]);//Output:a
+
+/*var obj = {prop1:"Prop 1", prop2:"Prop 2", prop3:["item 1 of prop 3", "item 2 of prop 3"]};
+console.log(obj);//Output:{
+  prop1: 'Prop 1',
+  prop2: 'Prop 2',
+  prop3: [ 'item 1 of prop 3', 'item 2 of prop 3' ]
+}*/
+//console.log(obj.prop3[1]);//Output:'item 2 of prop 3'
+
+
+//@Undefined, Empty values, null, NaN
+//var a;
+//console.log(a);//Output:undefined
+//console.log(a == undefined);//Output:true
+//console.log(a === undefined);//Output:true
+
+//a = null;
+//console.log(a);//Output:null
+//console.log(null == undefined);//Output:true
+//console.log(null === undefined);//Output:false
+
+
+//var b = "";
+//console.log(b);//Output:
+//console.log(b == a);//Output:false
+//console.log(null == "");//Output:false
+//console.log(undefined == "");//Output:false
+
+//var c = "abc"/10;
+//console.log(c);//Output:NaN
+//console.log(NaN == null);//Output:false
+//console.log(NaN == "");//Output:false
+
+//@Primitive and Reference Types
+  //Primitive Types
+  // number, string, boolean
+  
+  //deals with value
+  // different address
+//let a = 7;
+//let b = a;//Here b = 7
+
+//a = 45;
+//console.log(b);//Output: 7
+
+//reference types
+//array,object
+//same address
+//let numbers = [1, 2, 3];
+//let newNumbers = numbers;
+//console.log(newNumbers);//Output: [ 1, 2, 3 ]
+//numbers[1] = 500;
+//console.log(numbers);
+//console.log(newNumbers);
+
+//@Template literals (ES6)
+//console.log("First Line \nSecond Line");
+  //when using template literals
+  //Backtick
+//console.log(`First Line
+//Second Line`);
+
+//let a = `First Line 
+//Second Line`;
+//console.log(a);
+
+//let age = 36;
+//console.log ("His age is " + age);//Output:His age is 36
+//console.log (`His age is ${ age }`);//Output:His age is 36
+
+let name = "Kasem";
+let age = 36;
+let dob = "21 June, 1979";
+console.log(`His name is ${name}
+His age is ${age}
+Date of Birth ${dob}`);/*Output: His name is Kasem
+His age is 36
+Date of Birth 21 June, 1979*/
+
+let a = 34;
+let b = 67;
+console.log(`The result is ${a+b}`);//Output:The result is 101
+console.log(`${a}+${b} = ${ a+b }`);//Output:34+67 = 101
